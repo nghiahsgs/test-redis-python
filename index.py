88ed42c7f2bc5ddfe87ef92fd148d61fd58c1550
nghiahsgs <nghiahsgs@gmail.com>
2021-05-22 16:04:02 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import redis
 host = '127.0.0.1'
 port = '6379'
-client = redis.Redis(host=host,port =port)
+client = redis.Redis(host=host,port =port,decode_responses=True)
 name = client.get("name")
 print('name',name)
